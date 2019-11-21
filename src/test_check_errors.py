20527f412fe6c885cb99e6e97dd2721cbc127d29
Briand Paul <paul.briand@student-cs.fr>
2019-11-21 20:44:56 +0100

    Finished test_check_errors, even if it doesn't work

--- src/test_check_errors.py
+++ src/test_check_errors.py
@@ -5,13 +5,12 @@ from os.path import exists
 from os import remove
 
 def test_check_errors1():
-    valid_function = 'def new_grid():\n\treturn [['' for i in range(3)] for j in range(3)]'
-
+    valid_function = '''def new_grid():\n\treturn [['' for i in range(3)] for j in range(3)]'''
 
     user_code, name = get_name_of_function(valid_function)
 
     with open("src/function_to_test/function_to_test.py", "w") as fichier:
-        fichier.write('def function(L):\n' +  '\t' + user_code + '\treturn (' + name + '(*L))' )
+        fichier.write('def function(L):\n' + '\t' + user_code + '\treturn (' + name + '(*L))' )
 
     assert check_for_errors('1') == (True, "This function did not raise any error.\nThis function serves it's purpose. You can proceed to the next level")
 
@@ -19,17 +18,14 @@ def test_check_errors1():
     remove(path)
 
 def test_check_errors2():
-    invalid_function = 'def new_grid(a):\n\treturn [['' for i in range(3)] for j in range(3)]'
+    invalid_function = '''def new_grid(a):\n\treturn [['' for i in range(3)] for j in range(3)]'''
 
     user_code, name = get_name_of_function(invalid_function)
 
     with open("src/function_to_test/function_to_test.py", "w") as fichier:
-        fichier.write('def function(L):\n' +  '\t' + user_code + '\treturn (' + name + '(*L))' )
+        fichier.write('def function(L):\n' + '\t' + user_code + '\treturn (' + name + '(*L))' )
 
     assert check_for_errors('1') == (False, "You are applying some function or operator to a type it's not supposed to support.")
 
     path = "src/function_to_test/function_to_test.py"
     remove(path)
-
-
-    
