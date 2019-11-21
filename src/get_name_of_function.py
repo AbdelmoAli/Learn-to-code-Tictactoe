61748cd1dc2f2893d6c34eba7ff9c27983f9f343
Briand Paul <paul.briand@student-cs.fr>
2019-11-21 10:29:36 +0100

    Fixed get_name_of_function

---
+++ src/get_name_of_function.py
@@ -0,0 +1,18 @@
+def get_name_of_function(file):
+    new_file = file.split('\n')
+    name=""
+    L=[]
+    for row in new_file:
+        if row[:3] == 'def':
+            try:
+                i = row.index('(')
+            except:
+                L=[file]
+                name = ""
+                break
+            name = str(row[4:i])
+            L.append(row+'\n')
+        else:
+            L.append("\t"+row+'\n')
+    return "".join(L), name
+
